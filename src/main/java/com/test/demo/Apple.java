13fe8d8f3b9e4eee133204f2d22d100ca78cfffb
wz_glb <wz_glb@aliyun.com>
2020-02-26 21:24:02 +0800

    update from testGit

--- src/main/java/com/test/demo/Apple.java
+++ src/main/java/com/test/demo/Apple.java
@@ -4,5 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("apple...");
 		System.out.println("hello from local eclipse");
+		System.out.println("update from repository name is testGit");
 	}
 }
