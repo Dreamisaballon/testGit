83110e64f0c216b75ef23a49045caedb8dabb2de
WZ <WZ@aliyun.com>
2020-02-28 10:09:13 +0800

    第三次更新，从testGit1

--- src/main/java/com/test/demo/TestBranch.java
+++ src/main/java/com/test/demo/TestBranch.java
@@ -4,5 +4,6 @@ public class TestBranch {
 	public static void main(String[] args) {
 		System.out.println("新建分支hot_fix");
 		System.out.println("update...");
+		System.out.println("第三次更新from testGit1");
 	}
 }
